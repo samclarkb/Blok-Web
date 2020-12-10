e0afc24244fb40de26728c7536df8e1f15ca75b4
samclarkb <74268703+samclarkb@users.noreply.github.com>
2020-12-10 11:38:05 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 // JavaScript Document
+
+var knop = document.getElementById("hamburger");
+var kruisje = document.querySelector("header nav form a")
+var formulier = document.querySelector("form")
+
+
+function hamburgerOpenen(){
+  formulier.classList.add("toonmenu");
+}
+
+function hamburgerDicht(){
+  formulier.classList.remove("toonmenu")
+}
+
+kruisje.addEventListener("click", hamburgerDicht);
+
+knop.addEventListener("click", hamburgerOpenen);
